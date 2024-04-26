3d23d2c234b94bd9f64d5572dc6833f16d99daba
jchanquin-2022240 <jchanquin-2022240@kinal.edu.gt>
2024-04-25 23:02:39 -0600

    Creación de modelo y modelo terminado

---
+++ src/tasks/tareas.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const TareaSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    descripcion: {
+        type: String,
+        required: [true, "Description is required"],
+    },
+    feachaInicio: {
+        type: Date,
+        required: [true, "Start date is required"],
+    },
+    fechaCierre: {
+        type: Date,
+        required: [true, "End date is required"],
+    },
+    nombreResponsable: {
+        type: String,
+        required: [true, "Name of the responsible is required"],
+    },
+    estado: {
+        type: String,
+        enum: ['pendiente', 'terminada', 'cancelada'],
+        default: 'pendiente'
+    }
+})
+
+export default mongoose.model('Tarea', TareaSchema);
