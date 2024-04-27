0756ab6dbc05670e5aba7a20904ad2a424bf46b2
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-04-26 20:52:07 -0600

    add document routes and add router put

--- src/tasks/tareas.routes.js
+++ src/tasks/tareas.routes.js
@@ -10,3 +10,12 @@ import { existeTareaById } from '../helpers/db-validator.js';
 const router = Router();    
 
 
+router.put(
+    "/:id",
+    [
+      check("id", "No es un ID válido").isMongoId(),
+      check("id").custom(existeTareaById),
+      validarCampos,
+    ],
+    tareasPut
+  );
