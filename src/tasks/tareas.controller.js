5bac4b5aff55aafdef45b5b41e8e211c0af7130d
juz2019076 <juz-2019076@kinal.edu.gt>
2024-04-29 16:18:52 -0600

    Se agregan los metodos de Put y Delete

--- src/tasks/tareas.controller.js
+++ src/tasks/tareas.controller.js
@@ -1,5 +1,5 @@
 import { response, request } from "express";
-import Tarea from "./tarea.model.js";
+import Tarea from "./tareas.model.js";
 
 export const tareasPost = async (req, res = response) => {
     try {
@@ -49,3 +49,27 @@ export const tareasGet = async (req, res = response) => {
         res.status(500).json({ error: "Error interno del servidor" });
     }
 };
+
+export const tareasPut = async (req, res = response) => {
+    const { id } = req.params;
+    const { _id, ...resto } = req.body;
+
+    await Tarea.findByIdAndUpdate(id, resto);
+
+    const tarea = await Tarea.findOne({_id: id});
+
+    res.status(200).json({
+        msg: "Tarea actualizada",
+        tarea
+    });
+}
+
+export const  tareaDelete = async (req, res = response) => {
+    const { id } = req.params;
+
+    await Tarea.findByIdAndDelete(id, { estado: false});
+
+    res.status(200).json({
+        msg: "Tarea eliminada"
+    });
+}
